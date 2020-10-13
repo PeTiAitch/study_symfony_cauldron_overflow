9deacf5cfab7d4c8b0f827b6cea1c2e57940f840
plam <plam@localhost.dev>
2020-10-13 12:09:39 +0000

    Update Query & Rich vs Anemic Models

--- src/Entity/Question.php
+++ src/Entity/Question.php
@@ -112,4 +112,16 @@ class Question
         $prefix = $this->getVotes() >= 0 ? '+' : '-';
         return sprintf('%s %d', $prefix, abs($this->getVotes()));
     }
+
+    public function upVote(): self
+    {
+        $this->votes++;
+        return $this;
+    }
+
+    public function downVote(): self
+    {
+        $this->votes--;
+        return $this;
+    }
 }
